1a82566f32764453d1fb994757eb413a50531461
Chris <itschrislytle@gmail.com>
2019-03-20 16:43:47 -0700

    Server is connected to db

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');  
 
 const app = express();
 app.use(express.static(__dirname + '/client/public'));
+
+const officeQuery = "SELECT * FROM Devices.office;";
+
+const connection = mysql.createConnection({
+    host: '35.185.195.184',
+    user: 'student',
+    password: 'student',
+    database: 'Devices'
+});
+
+connection.connect(function (err) {
+    (err) ? console.log(err) : console.log(connection);
+});
+
+app.get('/Offices', (req, res) => {
+    connection.query(officeQuery, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        else {
+            return res.json({
+                data: results
+            });
+        }
+    });
+});
 
 app.get('/api/customers', (req, res) => {
   const customers = [
