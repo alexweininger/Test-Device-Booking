be20b42bf4e40977337aed543fa513585fc3a360
Daniel <danielborg291@gmail.com>
2019-03-19 20:11:49 -0700

    added color theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 import App from './App'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
-render(<App />, document.getElementById("root"));
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#F6F6F6',
+            dark: '#CE2B27'
+        },
+        secondary: {
+            main: '#CE2B27',
+        },
+    },
+});
+
+render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root')
+)
+
+
