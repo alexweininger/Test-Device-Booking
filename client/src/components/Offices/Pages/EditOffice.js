44de4cc7a909e819d74a83e286dff4e326767f4e
snook20 <snook20@up.edu>
2019-03-20 08:57:03 -0700

    fixedBug

--- client/src/components/Offices/Pages/EditOffice.js
+++ client/src/components/Offices/Pages/EditOffice.js
@@ -7,35 +7,34 @@ class EditOffice extends React.Component {
   
   render() {
     return (
-      <div>
-		<Button onClick>
-		<Button>
-			<ArrowBack />
-			Back to Office Info
-		</Button>
-		<h3>Edit Device Information</h3>
-		<form>
-        <TextField
-          id="Country"
-          label="Country"
-          margin="normal"
-		  defaultValue={this.props.office.country}
-        />
-		<TextField
-          id="City"
-          label="City"
-          margin="normal"
-		  defaultValue={this.props.office.city}
-		/>
-		<TextField
-          id="Address"
-          label="Address"
-          margin="normal"
-		  defaultValue={this.props.office.address}
-        />
-      </form>
-		<Button>Submit</Button>
-      </div>
+		<div>
+			<Button>
+				<ArrowBack />
+				Back to Office Info
+			</Button>
+			<h3>Edit Device Information</h3>
+			<form>
+				<TextField
+					id="Country"
+					label="Country"
+					margin="normal"
+					defaultValue={this.props.office.country}
+				/>
+				<TextField
+					id="City"
+					label="City"
+					margin="normal"
+					defaultValue={this.props.office.city}
+				/>
+				<TextField
+					id="Address"
+					label="Address"
+					margin="normal"
+					defaultValue={this.props.office.address}
+				/>
+				</form>
+			<Button>Submit</Button>
+		</div>
     );
   }
 }
