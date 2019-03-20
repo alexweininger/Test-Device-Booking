8f91ad8f8add92f5b63aaffe57fb29deec97393f
Daniel <danielborg291@gmail.com>
2019-03-19 20:33:00 -0700

    addjusted colors to match theme and be visible

--- client/src/components/Users/Pages/profile.js
+++ client/src/components/Users/Pages/profile.js
@@ -9,17 +9,17 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 const CustomTableCell = withStyles(theme => ({
-    head: {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
+  head: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
 const styles = theme => ({
-root: {
+  root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
@@ -36,48 +36,48 @@ root: {
 
 let id = 0;
 function createData(first_name, last_name, email, location, slack_name) {
-//   id += 1;
+  //   id += 1;
   return { first_name, last_name, email, location, slack_name };
 }
 
 const rows = [
-    createData('John', 'Snow', "knows.nothing@north.got", "the wall", "LordCommander2"),
-    createData('Bronius', null, null, null, null),
-  ];
+  createData('John', 'Snow', "knows.nothing@north.got", "the wall", "LordCommander2"),
+  createData('Bronius', null, null, null, null),
+];
 
-  function CustomizedTable(props) {
-    const { classes } = props;
-  
-    return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <CustomTableCell align="center">First Name</CustomTableCell>
-              <CustomTableCell align="center">Last Name</CustomTableCell>
-              <CustomTableCell align="center">Email</CustomTableCell>
-              <CustomTableCell align="center">Location</CustomTableCell>
-              <CustomTableCell align="center">Slack Name</CustomTableCell>
+function CustomizedTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <CustomTableCell align="center">First Name</CustomTableCell>
+            <CustomTableCell align="center">Last Name</CustomTableCell>
+            <CustomTableCell align="center">Email</CustomTableCell>
+            <CustomTableCell align="center">Location</CustomTableCell>
+            <CustomTableCell align="center">Slack Name</CustomTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow className={classes.row} key={row.id}>
+              <CustomTableCell align="center">{row.first_name}</CustomTableCell> {/*component="th" scope="row"*/}
+              <CustomTableCell align="center">{row.last_name}</CustomTableCell>
+              <CustomTableCell align="center">{row.email}</CustomTableCell>
+              <CustomTableCell align="center">{row.location}</CustomTableCell>
+              <CustomTableCell align="center">{row.slack_name}</CustomTableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => (
-              <TableRow className={classes.row} key={row.id}>
-                <CustomTableCell align="center">{row.first_name}</CustomTableCell> {/*component="th" scope="row"*/}
-                <CustomTableCell align="center">{row.last_name}</CustomTableCell>
-                <CustomTableCell align="center">{row.email}</CustomTableCell>
-                <CustomTableCell align="center">{row.location}</CustomTableCell>
-                <CustomTableCell align="center">{row.slack_name}</CustomTableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-    );
-  }
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
 
-  CustomizedTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(CustomizedTable);
+CustomizedTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CustomizedTable);
