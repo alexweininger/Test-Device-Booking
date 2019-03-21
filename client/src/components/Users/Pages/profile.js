ea76969ba21c65e25dfd3933d110ecdf5aed5821
Daniel <danielborg291@gmail.com>
2019-03-21 12:15:48 -0700

    added material-tables

--- client/src/components/Users/Pages/profile.js
+++ client/src/components/Users/Pages/profile.js
@@ -7,16 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
-const CustomTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
+import MaterialTable from 'material-table'
 
 const styles = theme => ({
   root: {
@@ -34,45 +25,53 @@ const styles = theme => ({
   },
 });
 
-let id = 0;
-function createData(first_name, last_name, email, location, slack_name) {
-  //   id += 1;
-  return { first_name, last_name, email, location, slack_name };
-}
-
-const rows = [
-  createData('John', 'Snow', "knows.nothing@north.got", "Portland, Oregon", "LordCommander2"),
-  createData('Bronius', null, null, null, null),
-];
-
 function CustomizedTable(props) {
   const { classes } = props;
 
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <CustomTableCell align="center">First Name</CustomTableCell>
-            <CustomTableCell align="center">Last Name</CustomTableCell>
-            <CustomTableCell align="center">Email</CustomTableCell>
-            <CustomTableCell align="center">Location</CustomTableCell>
-            <CustomTableCell align="center">Slack Name</CustomTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow className={classes.row} key={row.id} >
-              <CustomTableCell align="center">{row.first_name}</CustomTableCell> {/*component="th" scope="row"*/}
-              <CustomTableCell align="center">{row.last_name}</CustomTableCell>
-              <CustomTableCell align="center">{row.email}</CustomTableCell>
-              <CustomTableCell align="center">{row.location}</CustomTableCell>
-              <CustomTableCell align="center">{row.slack_name}</CustomTableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+    // <Paper className={classes.root}>
+    //   <Table className={classes.table}>
+    //     <TableHead>
+    //       <TableRow>
+    //         <CustomTableCell align="center">First Name</CustomTableCell>
+    //         <CustomTableCell align="center">Last Name</CustomTableCell>
+    //         <CustomTableCell align="center">Email</CustomTableCell>
+    //         <CustomTableCell align="center">Location</CustomTableCell>
+    //         <CustomTableCell align="center">Slack Name</CustomTableCell>
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {rows.map(row => (
+    //         <TableRow className={classes.row} key={row.id} >
+    //           <CustomTableCell align="center">{row.first_name}</CustomTableCell> {/*component="th" scope="row"*/}
+    //           <CustomTableCell align="center">{row.last_name}</CustomTableCell>
+    //           <CustomTableCell align="center">{row.email}</CustomTableCell>
+    //           <CustomTableCell align="center">{row.location}</CustomTableCell>
+    //           <CustomTableCell align="center">{row.slack_name}</CustomTableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </Paper>
+    <MaterialTable
+      columns={[
+        { title: 'First Name', field: 'firstName' },
+        { title: 'Last Name', field: 'lastName' },
+        { title: 'Email', field: 'email' },
+        { title: 'Location', field: 'location' },
+        { title: 'Slack Name', field: 'slackName' },
+      ]}
+      data={
+        [
+          { firstName: 'John', lastName: 'Snow', email: 'knows.nothing@north.got', location: 'Portland, Oregon', slackName: 'LordCommand' },
+          { firstName: 'Bronius', lastName: null, email: null, location: null, slackName: null },
+        ]}
+      title="Default Actions"
+      options={{
+        columnsButton: true,
+        exportButton: true,
+      }}
+    />
   );
 }
 
