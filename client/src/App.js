fcb9c170fdbbe73adbb3ddeab66809de180e6f59
giesta <47950470+giesta@users.noreply.github.com>
2019-03-24 18:53:33 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import TabMenu from './components/Layout/TabMenu';
+import Header from './components/Layout/Header';
+
+
+class App extends Component {
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="App">
+         <Header/>
+         <TabMenu/>
+      </div>
+    );
+  }
+}
+export default (App);
+
