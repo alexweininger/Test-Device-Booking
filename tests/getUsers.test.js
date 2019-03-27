327b224f8a725a555e84c57ac09d66a73a05cde0
alexweininger <alex.weininger@live.com>
2019-03-26 17:08:24 -0700

    tests for the /users route

--- tests/getUsers.test.js
+++ tests/getUsers.test.js
@@ -2,16 +2,34 @@ module.exports = require("babel-jest").createTransformer({
     rootMode: "upward",
   });
 
+const request = require('supertest');
+const app = require('../app');
 var getUsers = require('../routes/users/getUsers');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+describe('Tests if the server started successfully', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/helloWorld").then(response => {
+			expect(response.statusCode).toBe(200);
+        })
+    });
 });
 
-describe('test the getUsers route that returns a json array of all users' () => {
-	test('getUsers', () => {
-		expect('')
+describe('test the /getUsers route that returns a json array of all users', () => {
+	test('getUsers request returns OK', () => {
+		return request(app).post("/users").then(response => {
+            expect(response.statusCode).toBe(200);
+        });
+	});
+
+	test('getUsers request returns a defined object', () => {
+		return request(app).post("/users").then((response) => {
+            expect(response.body).toBeDefined();
+        });
+	});
+
+	test('getUsers request returns a an array with a length of at least 1', () => {
+		return request(app).post("/users").then((response) => {
+            expect(response.body.length).toBeGreaterThan(0);
+        });
 	});
 });
-
-it('')
