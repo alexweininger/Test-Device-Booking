46ff56f8e34846962446c2522fa089ed229767e3
alexweininger <alex.weininger@live.com>
2019-04-02 16:29:07 -0700

    passport routes

--- routes/users/passportRoute.js
+++ routes/users/passportRoute.js
@@ -9,17 +9,36 @@ app.post('/login', passport.authenticate('local-login', {
     failureFlash: true // allow flash messages
 
   }),
-    function (req, res) {
-  
+    (req, res) => {
       console.log("hello");
       if (req.body.remember) {
         req.session.cookie.maxAge = 1000 * 60 * 3;
       } else {
-  
         req.session.cookie.expires = false;
-  
       }
+      console.log('here');
       res.redirect('/');
-  
     });
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+      res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+      });
+    });
+
+    app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+    });
+}
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
 }
