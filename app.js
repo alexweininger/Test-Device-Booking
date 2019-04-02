22db77d71a7d8241253e6728849d18d567a41004
alexweininger <alex.weininger@live.com>
2019-04-02 16:29:32 -0700

    adding cors defs for logging in

--- app.js
+++ app.js
@@ -12,7 +12,28 @@ var cookieParser = require('cookie-parser');
 
 require('./config/passport')(passport); // pass passport for configuration
 
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cors());
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(express.static(__dirname + '/client/public'));
 
 //body parser for posts
