94f27d88aa47fd9ca0097b8fee7f1c7225bfadb7
hthoma20 <thoma20@up.edu>
2019-04-02 19:44:49 -0700

    bd query failing in new_office

--- routes/offices/new_office.js
+++ routes/offices/new_office.js
@@ -6,10 +6,22 @@ var db = require('../dbms.js');
 router.post('/', (req, res) => {
 	const office= req.body;
 	
-	db.dbqueryPromise('select * from office').then(results => {
+	//TODO make sure the office is unique
+	
+	let query= 'INSERT INTO office (Country, City, Address) ' +
+			   'VALUES ({country}, {city}, {address});';
+	
+	query= query.replace('{country}', office.country)
+		   .replace('{city}', office.city)
+		   .replace('{address}', office.address);
+	
+	console.log(query);
+	
+	db.dbqueryPromise(query).then(results => {
 		console.log(results);
 	}).catch(err => {
-		console.log("There was an error");
+		console.log("There was an error:");
+		console.log(err);
 	});
 				
 	res.sendStatus(200);
