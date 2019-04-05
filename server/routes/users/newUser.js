02102e5f2b3e6e9470bfb3e7a29cf33a2730542e
alexweininger <alex.weininger@live.com>
2019-04-05 16:52:53 -0700

    creating new user route

--- server/routes/users/newUser.js
+++ server/routes/users/newUser.js
@@ -1,7 +1,34 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const dbms = require('../dbms');
 
 const router = express.Router();
+
+// parse any type of request using bodyParser.json
+router.use(bodyParser.json({ type: '*/*' }));
+
+// Handles requests to create a new user.
+router.post('/', (req, res) => {
+	const user = req.body;
+
+	const err = isValidUser(user);
+
+	let err = isValidUser(user);
+
+	if (err) {
+		res.status(400).send(err);
+	} else {
+		const insert = 'INSERT INTO Users (firstName, lastName, email, slackUsername, id, officeId, role)';
+		const values = ` VALUES ('${user.firstName}', '${user.lastName}', '${user.email}', '${user.slackUsername}', '${user.id}', '${user.officeId}', '${user.role}');`
+		dbms.dbquery(insert + values, (err, results) => {
+			if (err) {
+				res.status(400).send(err);
+			} else {
+				res.status(200).send('User added successfully.');
+			}
+		});
+	}
+});
 
 function isValidUser(user) {
 	if (!user) {
@@ -16,22 +43,6 @@ function isValidUser(user) {
 	});
 	return err;
 }
-
-// parse any type of request using bodyParser.json
-router.use(bodyParser.json({ type: '*/*' }));
-
-// Handles requests to create a new user.
-router.post('/', (req, res) => {
-	const user = req.body;
-
-	const err = isValidUser(user);
-
-	if (err) {
-		res.status(400).send(err);
-	} else {
-		res.status(200).send('Profile created successfully.');
-	}
-});
 
 // user object:
 // {
