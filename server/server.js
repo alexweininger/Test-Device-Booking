49bb8e5ad3e25da5295e4385f907258388106a7d
giesta <47950470+giesta@users.noreply.github.com>
2019-04-11 23:43:50 +0300

    Delete server.js

--- server/server.js
+++
@@ -1,4 +0,0 @@
-// server.js simply starts the app.js file and listens on a port (5000)
-const app = require('./app');
-const port = 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
