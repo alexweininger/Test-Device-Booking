3e4a0ce9a8eedd1aa59a668552838f08ab4167a2
Niraj Mali <nirajlmali@gmail.com>
2019-04-15 19:37:01 -0700

    removed tests

--- client/tests/Users.test.js
+++ client/tests/Users.test.js
@@ -1,9 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/Users/Pages/User';
-
-@Test
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
