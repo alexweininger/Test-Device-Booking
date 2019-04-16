eaf7ee20449f924a8d697ebf2cc852536de61e1c
alexweininger <alex.weininger@live.com>
2019-04-16 13:55:41 -0700

    made sql query a constant

--- server/routes/users/getUsers.js
+++ server/routes/users/getUsers.js
@@ -3,33 +3,17 @@ var express = require('express');
 const dbms = require('../dbms');
 const router = express.Router();
 
-function SQLArrayToJSON(sql, callback) {
-  const arr = [];
-  Object.keys(sql).forEach(key => {
-    const rowObj = {};
-    const row = sql[key];
-    Object.keys(row).forEach(keyc => {
-      rowObj[keyc] = row[keyc];
-    });
-    arr.push(rowObj);
-  });
-  callback(arr);
-}
-
-// respond with "hello world" when a GET request is made to the homepage
 router.post('/', (req, res) => {
-
-	console.log('getUsers: request recieved');
-
 	// make call to db to get all users
-	dbms.dbquery('Select * from Users;', (err, results) => {
+	const queryString = 'Select * from Users;';
+	dbms.dbquery(queryString, (err, results) => {
 		if (err) {
 			console.error(err);
 			res.status(400).send(err);
 		} else {
 			// console.log('results', results);
 			res.setHeader('Content-Type', 'application/json');
-			SQLArrayToJSON(results, (json) => {
+			dbms.SQLArrayToJSON(results, (json) => {
 				res.status(200).json(json);
 			});
 		}
