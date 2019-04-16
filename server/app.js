05df498b24109c3aba2bd14bd5009513753fc647
alexweininger <alex.weininger@live.com>
2019-04-16 13:51:26 -0700

    commenting

--- server/app.js
+++ server/app.js
@@ -19,13 +19,7 @@ var cookieParser = require("cookie-parser");
 const dbms = require("./routes/dbms");
 
 connection.query(`USE ${dbconfig.database}`);
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
+
 passport.serializeUser(function(user, cb) {
 	console.log("serializeuser", user);
 
