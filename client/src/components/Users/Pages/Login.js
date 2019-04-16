22fed48e5eb4c52dff88e8ef3630bb5af20fd884
Daniel <danielborg291@gmail.com>
2019-04-16 11:52:39 -0700

    seperated email/ password fields for login/ signup

--- client/src/components/Users/Pages/Login.js
+++ client/src/components/Users/Pages/Login.js
@@ -136,12 +136,12 @@ class FullWidthTabs extends React.Component {
                                         Enter your details below to access your account:
                                     </h5>
                                     <FormControl margin="normal" required fullWidth>
-                                        <InputLabel htmlFor="username">Username</InputLabel>
-                                        <Input id="username" name="username" autoComplete="username" onChange={this.handleInputChange} />
+                                        <InputLabel htmlFor="email-login">Email</InputLabel>
+                                        <Input id="email-login" name="email-login" autoComplete="email-login" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <FormControl margin="normal" required fullWidth>
-                                        <InputLabel htmlFor="password">Password</InputLabel>
-                                        <Input name="password" type="password" id="password" autoComplete="current-password" onChange={this.handleInputChange} />
+                                        <InputLabel htmlFor="password-login">Password</InputLabel>
+                                        <Input name="password-login" type="password" id="password-login" autoComplete="current-password" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <Button
                                         type=""
@@ -174,8 +174,8 @@ class FullWidthTabs extends React.Component {
                                         <Input value={this.state.lastName} id="lastName" name="lastName" autoComplete="lastName" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <FormControl margin="normal" required fullWidth>
-                                        <InputLabel htmlFor="email">Email</InputLabel>
-                                        <Input id="email" name="email" autoComplete="email" onChange={this.handleInputChange} />
+                                        <InputLabel htmlFor="email-signup">Email</InputLabel>
+                                        <Input id="email-signup" name="email-signup" autoComplete="email-signup" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <FormControl margin="normal" required fullWidth>
                                         <InputLabel htmlFor="slackUsername">Slack Username</InputLabel>
@@ -190,8 +190,8 @@ class FullWidthTabs extends React.Component {
                                         <Input id="officeId" name="officeId" autoComplete="officeId" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <FormControl margin="normal" required fullWidth>
-                                        <InputLabel htmlFor="password">Password</InputLabel>
-                                        <Input name="password" type="password" id="password" autoComplete="current-password" onChange={this.handleInputChange} />
+                                        <InputLabel htmlFor="password-signup">Password</InputLabel>
+                                        <Input name="password-signup" type="password" id="password-signup" autoComplete="current-password" onChange={this.handleInputChange} />
                                     </FormControl>
                                     <Button
                                         type=""
