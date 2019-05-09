a862baa9260577fed0c4b4ca4956be8cc36716c8
giesta <47950470+giesta@users.noreply.github.com>
2019-05-09 15:38:30 +0300

    /server/config/database.js

--- server/config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  connection: {
-    host: "35.185.195.184",
-    user: "student",
-    password: "Ktustudent"
-  },
-  database: "Device_Booking",
-  user_table: "atbl_Users"
-};
