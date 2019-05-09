e818a35295e2efc8dbd116b5154d9c27d5759afc
giesta <47950470+giesta@users.noreply.github.com>
2019-05-09 08:27:34 +0300

    changed query

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-    'connection': {
-        'host': '35.185.195.184',
-        'user': 'student',
-        'password': 'student'
-    },
-        'database': 'Users',
-        'user_table': 'Users'
+  connection: {
+    host: "35.185.195.184",
+    user: "student",
+    password: "student"
+  },
+  database: "Device_Booking",
+  user_table: "atbl_Users"
 };
