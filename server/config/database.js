78a820f1d8cfa35d7853ae8fa3320b79dbea6c82
giesta <47950470+giesta@users.noreply.github.com>
2019-05-09 15:27:36 +0300

    changed something

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   connection: {
     host: "35.185.195.184",
     user: "student",
-    password: "student"
+    password: "Ktustudent"
   },
   database: "Device_Booking",
   user_table: "atbl_Users"
