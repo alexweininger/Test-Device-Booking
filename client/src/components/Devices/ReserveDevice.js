6438a44ab7f56d48a8c1d3a495f96bf5e8867478
agnmol <moleikaityte.agne@gmail.com>
2019-05-21 13:26:37 +0300

    reserve device

--- client/src/components/Devices/ReserveDevice.js
+++ client/src/components/Devices/ReserveDevice.js
@@ -6,15 +6,10 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { withStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
-import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
-import BookingsTable from "./BookingsTable";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
-import NewDevice from "../../App";
 import DateFnsUtils from "@date-io/date-fns";
-import ReactDOM from 'react-dom';
-import { DateRangePicker } from 'material-ui-datetime-range-picker';
 //import ReservationsTable from "./ReservationsTable";
 import {
   MuiPickersUtilsProvider,
@@ -24,9 +19,6 @@ import {
 
 var today = new Date();
 var time = [];
-var ID = "0";
-var bkngs = [];
-var closestBooking;
 
 const styles = theme => ({
   dialog: {
@@ -62,6 +54,7 @@ class ReserveDevice extends React.Component {
     super(props);
 
     this.state = {
+      closestBooking: [],
       bookings: [],
       time: [],
       open: false,
@@ -78,14 +71,20 @@ class ReserveDevice extends React.Component {
   }
 
   handleDateChangeFrom = date => {
+    time=[];
+    if(date.getDate() != today.getDate() || 
+      date.getFullYear() != today.getFullYear() || 
+      date.getMonth() != today.getMonth()){
+        date.setHours(0, 0, 0);
+    }
     console.log("dateChanging");
     this.setState({selectedDateTo: date});
     this.setState({selectedDateFrom: date});
     console.log("dateChanged");
     console.log("Accept");
-    this.setState({ bookings: this.getTodaysBookings(timeArrayFrom)});
-    this.render();
-    console.log(this.state.selectedDateFrom);
+    this.setState({ bookings: this.getBookings(date, timeArrayFrom)});
+   // this.render();
+   // console.log(this.state.selectedDateFrom);
     
     console.log("Accepted");
 };
@@ -93,6 +92,13 @@ handleDateChangeTo = date => {
   console.log(this.state.selectedDateFrom);
     this.setState({selectedDateTo: date});
 };
+  handleTimeChangeFrom = event =>{
+    this.setState({selectedDateFrom: event.target.value});
+    this.setState({closestBooking: this.getClosestBooking});
+  }
+  handleTimeChangeTo = event =>{
+    this.setState({selectedDateTo: event.target.value});
+  }
   handleClickOpen = () => {
     this.setState({ open: true });
   };
@@ -107,10 +113,6 @@ handleDateChangeTo = date => {
     
   };
     
-  disableRandomDates() {
-    console.log(Math.random() > 0.7);
-    return Math.random() > 0.7;
-  }
   disableWeekends(date) {
       return date.getDay() === 0 || date.getDay() === 6;
   }
@@ -241,9 +243,10 @@ handleDateChangeTo = date => {
       </div>
     );
   }
-  getTodaysBookings(callback) {
-    const request = new Request(
-      `/get_dayBookings/${this.state.reserved.sNumber}`,
+  getBookings(date, callback) {
+    console.log(date+"getBookings");
+    var query = createQueryDayBookings(date, this.props.sNumber)
+    const request = new Request(`/get_Booking/` + query,
       {
         method: "GET"
       }
@@ -257,8 +260,7 @@ handleDateChangeTo = date => {
   
             this.setState({ bookings: obj });
             console.log("loaded all bookings ReserveDevice");
-            callback(this.state.selectedDateFrom, this.state.bookings)
-            ;
+            callback(date, this.state.bookings);
             return obj;
           });
         }
@@ -268,79 +270,114 @@ handleDateChangeTo = date => {
         console.log("get failed");
       });
   }
-  getClosestBooking() {
-    const request = new Request(
-      `/get_closestBooking/${this.state.reserved.sNumber}`,
-      {
-        method: "GET"
-      }
-    );
-  
+  getClosestBooking(date) {
+    console.log("closestBooking");
+    var query = createQueryClosestBooking(date, this.props.sNumber);
+    const request = new Request("/get_closestBooking/" + query, {
+      method: "GET"
+    });
+
     fetch(request)
       .then(res => {
         if (res.ok) {
           res.json().then(obj => {
             console.log(obj);
-  
-            this.setState({ bookings: obj });
-            console.log("loaded all bookings ReserveDevice");
+
+            this.setState({ closestBooking: obj });
+            console.log("loaded closest booking");
             return obj;
           });
         }
       })
       .catch(err => {
-        console.log("Error in getTodaysBookings", err);
+        //if we successfully updated the DB
+        console.log("Error in getDevices", err);
         console.log("get failed");
       });
   }
 }
 
+function createQueryClosestBooking(date, id){
+  return("WHERE year(StartDate)>"+date.getFullYear()+
+  " AND month(StartDate)>"+(date.getMonth()+1)+
+  " AND day(StartDate)>"+date.getDate()+
+  " AND fk_device_ser_nr="+id);
+}
+function createQueryDayBookings(date, id){
+  return("WHERE year(StartDate)="+date.getFullYear()+
+  " AND month(StartDate)="+(date.getMonth()+1)+
+  " AND day(StartDate)="+date.getDate()+
+  " AND year(FinishDate)="+date.getFullYear()+
+  " AND month(FinishDate)="+(date.getMonth()+1)+
+  " AND day(FinishDate)="+date.getDate()+
+  " AND fk_device_ser_nr="+id);
+}
 function timeArrayFrom(d, bookings) {
 
   var currentDate = new Date();
   var bookingUntilDate = new Date();
   bookingUntilDate.setDate(d.getDate() + 1);
-  bookingUntilDate.setHours(0);
-  bookingUntilDate.setMinutes(0);
-
-  if(d.getDate() === currentDate.getDate() && currentDate.getFullYear() === d.getFullYear() && currentDate.getMonth() === d.getMonth())
+  bookingUntilDate.setHours(0, 0, 0);
+  if(d.getDate() != currentDate.getDate() || currentDate.getFullYear() != d.getFullYear() || currentDate.getMonth() != d.getMonth())
   {
-    var min = currentDate.getMinutes();
+    currentDate.setFullYear(d.getFullYear(), d.getMonth(), d.getDate());
+    currentDate.setHours(0, 0, 0);
+  }
+  else{
+    var min = d.getMinutes();
     min = (Math.ceil(min / 15) + 1) * 15;
     if (min > 60) {
-      currentDate.setHours(currentDate.getHours() + 1, 15, 0, 0);
+      currentDate.setHours(d.getHours() + 1, 15, 0, 0);
     } else {
       currentDate.setMinutes(min, 0, 0);
     }
   }
-  var i = 0;
-  var element = bookings[i];
-  var s=element.StartDate;
-  var closestBooking = new Date()
-  closestBooking.setFullYear(s.substring(0,4), s.substring(5,7)-1, s.substring(8,10));
-  closestBooking.setHours(s.substring(11, 13), s.substring(14, 16), 0, 0);
+  
+  
+  if(bookings.length > 0)
+  {
+    console.log("bookings");
+    console.log(currentDate);
+    console.log("bookingslength>0");
+    var i = 0;
+    var element = bookings[i];
+    var s=element.StartDate;
+    var closestBooking = new Date()
+    closestBooking.setFullYear(s.substring(0,4), s.substring(5,7)-1, s.substring(8,10));
+    closestBooking.setHours(s.substring(11, 13), s.substring(14, 16), 0, 0);
+    console.log(s+" s");
+    console.log(closestBooking+" closest");
 
-  while(currentDate <= bookingUntilDate){
-    console.log(closestBooking-currentDate+" b-c"+closestBooking.getHours()+":"+closestBooking.getMinutes()+" "+currentDate.getHours()+":"+currentDate.getMinutes());
-    if(closestBooking-currentDate <= 15 && closestBooking.getHours() != 0)
-    {
-      console.log("<15");
-      currentDate.setHours(element.FinishDate.substring(11, 13), element.FinishDate.substring(14, 16));
-      if(i+1 < bookings.length){
-        i++;
-        element = bookings[i];
-      console.log(element.StartDate+" element <15");
-      s=element.StartDate;
-      closestBooking.setFullYear(s.substring(0,4), s.substring(5,7)-1, s.substring(8,10));
-      closestBooking.setHours(s.substring(11, 13), s.substring(14, 16), 0, 0);
+    while(currentDate <= bookingUntilDate){
+      console.log(closestBooking-currentDate+" b-c"+closestBooking.getHours()+":"+closestBooking.getMinutes()+" "+currentDate.getHours()+":"+currentDate.getMinutes());
+      if(closestBooking-currentDate <= 15 && closestBooking.getHours() != 0)
+      {
+  //      console.log("<15");
+        currentDate.setHours(element.FinishDate.substring(11, 13), element.FinishDate.substring(14, 16));
+        if(i+1 < bookings.length){
+          i++;
+          element = bookings[i];
+   //     console.log(element.StartDate+" element <15");
+        s=element.StartDate;
+        closestBooking.setFullYear(s.substring(0,4), s.substring(5,7)-1, s.substring(8,10));
+        closestBooking.setHours(s.substring(11, 13), s.substring(14, 16), 0, 0);
+        }
+        else{
+          closestBooking.setFullYear(bookingUntilDate.getFullYear(), bookingUntilDate.getMonth(), bookingUntilDate.getDate());
+          closestBooking.setHours(bookingUntilDate.getHours(), bookingUntilDate.getMinutes(), 0, 0);
+        }
       }
-      else{
-        closestBooking.setFullYear(bookingUntilDate.getFullYear(), bookingUntilDate.getMonth(), bookingUntilDate.getDate());
-        closestBooking.setHours(bookingUntilDate.getHours(), bookingUntilDate.getMinutes(), 0, 0);
-      }
+        time.push(currentDate);
+        currentDate = new Date(currentDate.getTime() + 15 * 60000);
+      
     }
-    time.push(currentDate);
-    currentDate = new Date(currentDate.getTime() + 15 * 60000);
+  }
+  else{
+    console.log(currentDate+" cd");
+    while(currentDate <= bookingUntilDate){
+      time.push(currentDate);
+      currentDate = new Date(currentDate.getTime() + 15 * 60000);
+    }
   }
 
   console.log("timearray created");
