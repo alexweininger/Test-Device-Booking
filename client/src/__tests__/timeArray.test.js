a801d0c0227fc73776abb028fca1472584f1fdec
agnmol <moleikaityte.agne@gmail.com>
2019-05-26 21:26:51 +0300

    BookDevice connected to user

--- client/src/__tests__/timeArray.test.js
+++ client/src/__tests__/timeArray.test.js
@@ -1,15 +1,15 @@
-const { timeArray_test } = require("../components/Devices/BookDevice");
+const { timeArray_test } = require("../components/Devices/TestFunctions");
 
-test("first time in timeArray", () => {
+test("first time in timeArray when time is 10:54:30", () => {
   const date = new Date(2018, 11, 24, 10, 54, 30, 0);
   const time = timeArray_test(date, 1);
   expect(time[0].getHours().toString()).toEqual("11");
   expect(time[0].getMinutes().toString()).toEqual("15");
 });
 
-test("last time in timeArray", () => {
-  const date = new Date(2018, 11, 24, 10, 54, 30, 0);
+test("first time in timeArray when time is 09:01:10", () => {
+  const date = new Date(2015, 5, 12, 9, 1, 10, 0);
   const time = timeArray_test(date, 1);
-  expect(time[time.length - 1].getHours().toString()).toEqual("0");
-  expect(time[time.length - 1].getMinutes().toString()).toEqual("0");
+  expect(time[0].getHours().toString()).toEqual("9");
+  expect(time[0].getMinutes().toString()).toEqual("30");
 });
